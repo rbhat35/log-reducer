aa101effc45c8fc0b789cf4904821d51e2f31808
Joey Allen <jallen309@gatech.edu>
2018-08-22 16:43:28 -0400

    Added neo4j setup.

---
+++ parser/neo4j-load-csv.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "usage: ./neo4j-load-csv.sh data/csv-folder &> out"
+    exit 2
+fi
+
+if [[ `id -u` -ne 0 ]]; then
+    echo "Need to be root"
+    exit 1
+fi
+
+# Get parameter
+root=$1
+IMPORT_DIR="/data/neo4j-csvs"
+CYPHER_BIN="cypher-shell"
+NEO4J_SERVER="143.215.130.71:7687"
+USER="neo4j"
+CYPHER_ARGS="-a $NEO4J_SERVER -u neo4j -p theianeo4j1"
+
+# QUERY="\"CREATE CONSTRAINT ON (n:NODE) ASSERT n.uuid IS UNIQUE\""
+#eval "${CYPHER_BIN}" "${CYPHER_ARGS}" "${QUERY}"
+
+INPUT_FILE='test.csv'
+
+# Get list of CSV files
+#for file in $root/backward-edge-*; do
+#    if [[ -f $file ]]; then
+#        echo $file
+#
+#	# Copy file
+cp $INPUT_FILE $IMPORT_DIR/backward-edge.csv
+
+QUERY="\"
+USING PERIODIC COMMIT 500
+LOAD CSV FROM 'file:///backward-edge.csv' as line
+MERGE (n1:SUBJECT {name: line[1]})
+MERGE (n2:RESOURCE {name: line[4], inode: line[3]})
+WITH line,n1,n2
+CREATE (n1)<-[:SYSCALL {nodeType:line[2], type : line[2]}]-(n2)
+\""
+
+#WITH line,n1,n3
+DELETE="MATCH (n) DETACH DELETE n;"
+eval "${CYPHER_BIN}" "${CYPHER_ARGS}" "${DELETE}"
+eval "${CYPHER_BIN}" "${CYPHER_ARGS}" "${QUERY}"
+
+#	QUERY2="\"
+#	USING PERIODIC COMMIT 500
+#	LOAD CSV FROM 'file:///backward-edge.csv' as line
+#	MERGE (n1:NODE {uuid: line[3]})
+#	MERGE (n2:NODE {uuid: line[4]})
+#	MERGE (n3:NODE {uuid: line[5]})
+#	WITH line,n1,n2,n3
+#	#WHERE n1.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0' AND n2.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
+#	CREATE (n1)<-[:NODE {uuid:line[3], nodeType:line[0], type:line[2], ts:line[6], size:line[7], name:line[8]}]-(n2)
+#	WITH line,n1,n3
+#	WHERE n1.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0' AND n3.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
+#	CREATE (n1)<-[:NODE {uuid:line[3], nodeType:line[0], type:line[2], ts:line[6], size:line[7], name:line[8]}]-(n3);
+#	\""
+
+	# Load CSV file into Neo4j
+#	time eval "${CYPHER_BIN}" "${CYPHER_ARGS}" "${QUERY}"
+#    fi
+# done
--- parser/parser.py
+++ parser/parser.py
@@ -3,8 +3,66 @@ import auparse
 import audit
 import sys
 import os
+from os import path
+import json
+from collections import defaultdict
+
+class ParseError(Exception):
+    pass
+
+file_map = defaultdict(dict)
+i_map = defaultdict(dict)
 
 
+with open('./sys_table.json') as infile:
+    sys_table = json.load(infile)
+
+
+def get_subject(au):
+    pid = au.find_field('pid')
+    exe = au.find_field('exe')
+
+    subject = "{0}:{1}".format(pid, exe)
+    return (subject, pid, exe)
+
+def get_ts(au):
+    event = au.get_timestamp()
+    ts = "{0}.{1}.{2}".format(event.sec, event.milli, event.serial)
+    return ts
+
+def get_rc(au):
+    return au.find_field('exit')
+
+
+def handle_new(au):
+    """ Add resource descriptor to map.
+
+    XXX. Order of fields matter when using find_field.
+
+    1. Adds a mapping from <pid>:fd --> inode.
+    """
+    fd = get_rc(au)
+    items = au.find_field('items')
+    subject, pid, _ = get_subject(au)
+
+    key = "{0}:{1}".format(pid, fd)
+    # Get CWD.
+    au.next_record()
+    cwd = au.find_field('cwd')
+    au.next_record()
+    name = au.find_field('name')
+    inode = hex(int(au.find_field('inode')))
+
+    i_map[inode] = name if '.' != name[0] else path.join(cwd + name[1:])
+    file_map[key] = inode
+
+    return (subject, inode)
+
+
+def get_inode(pid, fd):
+    """Get inode value for pid, fd pair."""
+    key = "{0}:{1}".format(pid, fd)
+    return file_map[key]
 
 
 def parse_syscall(au):
@@ -13,30 +71,31 @@ def parse_syscall(au):
     Warning: Order of parsing matters.
     """
     sysnum = au.find_field('syscall')
-    pid = au.find_field('pid')
-    exe = au.find_field('exe')
-    subject = "{0}:{1}".format(pid, exe)
-    event = au.get_timestamp()
-    item = au.find_field('item')
+    syscall = sys_table[sysnum]
+    ts = get_ts(au)
+    if syscall in ['open', 'execve']:
+        # Create file map and inode map.
+        subject, resource  = handle_new(au)
+        #fd = au.find_field('exit')
+        event = (ts, subject, syscall, resource, i_map[resource])
+    elif syscall in ['read', 'write']:
+        fd = au.find_field('a0')
+        subject, pid, _ = get_subject(au)
+        inode = get_inode(pid, fd)
+        event = (ts, subject, syscall, get_inode(pid, fd), i_map[inode])
+    else:
+        return
 
-    ts = "{0}.{1}.{2}".format(event.sec, event.milli, event.serial)
-    event = (ts, subject, sysnum, item)
-    print sysnum
-    print event
+    print ','.join(event)
 
 
 def parse_file(au_log):
-
     au = auparse.AuParser(auparse.AUSOURCE_FILE, au_log)
 
-    while True:
-        au.parse_next_event()
-        #print au.get_type(), audit.audit_msg_type_to_name(au.get_type())
+    while au.parse_next_event():
         if au.get_type() == 1300:
             parse_syscall(au)
-        #print audit.audit_msg_type_to_name(au.get_type()), au.get_type()
         au.next_record()
-
 
 
 def main():
@@ -44,8 +103,5 @@ def main():
     au_log = sys.argv[1]
     parse_file(au_log)
 
-
-
 if __name__ == '__main__':
     main()
-
