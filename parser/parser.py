4ccb9299bb2176c3d1e0e10da3c453c48b38b0a9
Joey Allen <jallen309@gatech.edu>
2018-08-29 12:09:28 -0400

    Adding parse code.

--- parser/parser.py
+++ parser/parser.py
@@ -16,6 +16,13 @@ i_map = defaultdict(dict)
 
 def get_inode(pid, fd):
     """Get inode value for pid, fd pair."""
+    s = {'0': 'stdin', '1' : 'stdout', '2' : 'stderr'}
+
+    i_map[pid].update(s)
+
+    if fd in ['0', '1', '2']:
+        return s[fd]
+
     key = "{0}:{1}".format(pid, fd)
     return file_map[key]
 
@@ -24,7 +31,6 @@ def get_ts(au):
     event = au.get_timestamp()
     ts = "{0}.{1}.{2}".format(event.sec, event.milli, event.serial)
     return ts
-
 
 def get_rc(au):
     return au.find_field('exit')
@@ -56,13 +62,18 @@ class Parser(object):
         sysnum = au.find_field('syscall')
         self.syscall = sys_table[sysnum]
 
+        print au.get_line_number()
+
         if self.syscall in ['open', 'execve']:
             event = self.handle_open(au)
             #event = (ts, subject, self.syscall, resource, i_map[resource])
         elif self.syscall in ['read', 'readv']:
             event = self.handle_read(au)
         elif self.syscall in ['write', 'writev']:
-            event = self.handle_write(au)
+            event = 'not used.'
+            #event = self.handle_write(au)
+        elif self.syscall in ['close']:
+            event = self.handle_close(au)
         else:
             event = "not used."
 
@@ -78,16 +89,27 @@ class Parser(object):
         1. Adds a mapping from <pid>:fd --> inode.
         """
         fd = get_rc(au)
+
+        if int(fd) < 0:
+            return (None, None)
+
+
         items = au.find_field('items')
         subject, pid, _ = get_subject(au)
 
         key = "{0}:{1}".format(pid, fd)
+
         # Get CWD.
         au.next_record()
         cwd = au.find_field('cwd')
         au.next_record()
         name = au.find_field('name')
-        inode = hex(int(au.find_field('inode')))
+
+        if fd not in ['0', '1', '2']:
+            inode = hex(int(au.find_field('inode')))
+        else:
+            # stdin, stdout, ...
+            inode = fd
 
         i_map[inode] = name if '.' != name[0] else path.join(cwd + name[1:])
         file_map[key] = inode
@@ -116,8 +138,24 @@ class Parser(object):
         """syscalls open"""
         ts = get_ts(au)
         subject, resource = self.handle_new(au)
+        if not subject:
+            return None
         event = (ts, subject, self.syscall, resource, i_map[resource])
         return event
+
+    def handle_close(self, au):
+        """syscalls open"""
+        ts = get_ts(au)
+        fd = au.find_field('a0')
+        subject, pid, _ = get_subject(au)
+        inode = get_inode(pid, fd)
+        if inode not in ['stdin', 'stdout', 'stderr']:
+            filename = i_map[inode]
+        else:
+            filename = "stdout"
+        event = (ts, subject, self.syscall, inode, filename)
+        return event
+
 
 
 def main():
