d7c570e24bb3fad968817101dba10a9e084334ee
Joey <jallen309@gatech.edu>
2018-09-12 11:54:06 -0400

    Add trace.sh script

---
+++ examples/trace.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+trace="sudo /home/joey/log-compression-project/tracer/bin/trace"
+
+run_autrace() {
+    sudo auditctl -D
+    cmd=$1
+    out=$2
+    pid=$($trace $cmd -r | grep -o "You can locate the records with .*" | grep -oE "[0-9]+")
+    echo "pid" $pid
+    sudo ausearch -r -p $pid  > $out
+}
+
+sudo auditctl -D
+# Demonstrates the affect of loops on provenance graph.
+run_autrace $1 $2
+
