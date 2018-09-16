383d74c36d65001f5ac8da6fb763fd2507c68122
RB <rsbhat2@gmail.com>
2018-09-16 13:33:47 -0400

    Added a shell script to run the python file

---
+++ examples/run_python_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+trace="sudo /home/joey/log-compression-project/tracer/bin/trace"
+
+run_autrace() {
+    sudo auditctl -D
+    cmd=$1
+    out=$2
+    pid=$($trace $cmd -r | grep -oE "[0-9]+")
+    echo "pid" $pid
+    sudo ausearch -r -p $pid  > $out
+}
+
+sudo auditctl -D
+# Demonstrates the affect of loops on provenance graph.
+run_autrace "/usr/bin/python ../tests/test_case2.py" "test_case2.audit"
+# run_autrace "./readloop 1024.txt" "readloop_1024.audit" 
+
