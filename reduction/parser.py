505028e8477b44d89b564d50bbb93a58902fae1a
Sanya Chaba <sanyachaba26@gmail.com>
2018-10-18 21:23:22 -0400

    changed paths in parser.py

--- reduction/parser.py
+++ reduction/parser.py
@@ -3,8 +3,8 @@ import string
 from collections import defaultdict, OrderedDict
 
 
-FORWARD_CSV_PATH = "/Users/sanyachaba/Desktop/GATECH/Fall 2018/Research/log_compression/parser/forward.csv"
-BACKWARD_CSV_PATH = "/Users/sanyachaba/Desktop/GATECH/Fall 2018/Research/log_compression/parser/backwards.csv"
+FORWARD_CSV_PATH = "/home/raghav/log-compression-project/parser/forward.csv"
+BACKWARD_CSV_PATH = "/home/raghav/log-compression-project/parser/backwards.csv"
 
 def read_csv(PATH):
     f_csvfile = open(PATH, 'rb')
@@ -29,36 +29,42 @@ def parser():
     b_row = next(b, "DONE")
 
     while f_row is not "DONE" or b_row is not "DONE":
-	f_milli_ind = f_row[0].rfind(".")
-	b_milli_ind = b_row[0].rfind(".")
-	f_serial_ind = f_row[0].rfind(":")
-	b_serial_ind = b_row[0].rfind(":")
+        if f_row is not "DONE":
+            f_milli_ind = f_row[0].rfind(".")
+            f_serial_ind = f_row[0].rfind(":")
+            # f_start = float(f_row[0][:f_milli_ind] + f_row[0][f_milli_ind + 1:f_serial_ind] + "." + f_row[0][f_serial_ind + 1:])
+            f_start = float(f_row[0][f_serial_ind + 1:])
+        if b_row is not "DONE":
+            b_milli_ind = b_row[0].rfind(".")
+            b_serial_ind = b_row[0].rfind(":")
+            # b_start = float(b_row[0][:b_milli_ind] + b_row[0][b_milli_ind + 1:b_serial_ind] + "." + b_row[0][b_serial_ind + 1:])
+            b_start = float(b_row[0][b_serial_ind + 1:])
 
-	f_start = float(f_row[0][:f_milli_ind] + f_row[0][f_mili_ind + 1:f_serial_ind] + "." + f_row[f_serial_ind + 1:])
-	b_start = float(b_row[0][:b_milli_ind] + b_row[0][b_mili_ind + 1:b_serial_ind] + "." + b_row[b_serial_ind + 1:])
 
         if b_row is "DONE" or (f_row is not "DONE" and f_start <= b_start):
             v = f_row[1]
             u = f_row[4]
             sys_call = f_row[2]
             time_start = f_start
+            metaData = (f_row[0], f_row[3], "FORWARD")
             f_row = next(f, "DONE")
-	    metaData = (f_row[0], f_row[3], "FORWARD")
 
         else:
             v = b_row[4]
             u = b_row[1]
             sys_call = b_row[2]
             time_start = b_start
+            metaData = (b_row[0], b_row[3], "BACKWARD")
             b_row = next(b, "DONE")
-	    metaData = (b_row[0], b_row[3], "BACKWARD")
 
         parents[v].append((u, sys_call, id))
         children[u].append((v, sys_call, id))
         events[(u, v, sys_call, id)] = (time_start, time_start)
-	meta[id] = metaData
+    	meta[id] = metaData
         id += 1
 
 
 
     return parents, children, events, meta
+
+parser()
