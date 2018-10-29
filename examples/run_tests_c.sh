428a97a4f2fb1beb77319cbcac7abec5ab387dcc
Sanya Chaba <sanyachaba26@gmail.com>
2018-10-29 12:45:57 -0400

    shell script for c test cases

---
+++ examples/run_tests_c.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+trace="sudo /home/sanya/log-compression-project/tracer/bin/trace"
+
+run_autrace() {
+    sudo auditctl -D
+    cmd=$1
+    out=$2
+    pid=$($trace $cmd -r | grep -oE "[0-9]+")
+    echo "pid" $pid
+    sudo ausearch -r -p $pid  > $out
+}
+
+sudo auditctl -D
+# Demonstrates the affect of loops on provenance graph.
+run_autrace "../c_tests/testCase1.c" "testCase1.audit"
