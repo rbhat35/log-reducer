f2f731c86f544f6f3da5c33fe0f862e35816b3dc
rbhat35 <rsbhat2@gmail.com>
2018-10-30 12:50:48 -0400

    Added two new testCases

---
+++ c_tests/testCase4.c
@@ -0,0 +1,29 @@
+// opens, reads, and write a file.
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+    int fd;
+    int rc = 0;
+    int bytes_read = 0;
+    char buffer[2056];
+
+    write(1, buffer, bytes_read);
+    write(1, buffer, bytes_read);
+
+    fd = open(argv[1], O_RDWR);
+    // Read 24 bytes at a time.
+    do {
+        bytes_read += rc;
+        rc = read(fd, buffer + bytes_read, 128);
+    } while (rc > 0);
+
+    buffer[bytes_read + 1] = '\0';
+
+    write(1, buffer, bytes_read);
+    write(1, buffer, bytes_read);
+}
