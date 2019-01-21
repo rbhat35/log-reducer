c83d5ab717488d95635fb9d0b16a332c96ed730b
RB <rsbhat2@gmail.com>
2019-01-21 14:56:08 -0500

    Updated start_audit.sh to start service. Added 12 second log

--- audit/start_audit.sh
+++ audit/start_audit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo rm /var/log/audit/audit.log
+sudo systemctl stop auditd
 
 chown root:root audit.rules
 chmod 0600 audit.rules
@@ -7,3 +9,5 @@ chmod 0600 audit.rules
 auditctl -D
 auditctl -R audit.rules
 auditctl -l
+
+sudo systemctl start auditd
