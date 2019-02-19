11cb6ffb90087aecae0e0b2ed709202db3298815
rbhat35 <rsbhat2@gmail.com>
2019-02-19 01:30:45 -0500

    Added necessary argument to C Test Cases

--- c_tests/runTestCases.sh
+++ c_tests/runTestCases.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./testCase1.o
-./testCase2.o
-./testCase3.o
-./testCase4.o
-./testCase5.o
-./testCase6.o
-./testCase7.o
-./testCase8.o
-./testCase9.o
+./testCase1.o "test.txt"
+./testCase2.o "test.txt"
+./testCase3.o "test.txt"
+./testCase4.o "test.txt"
+./testCase5.o "test.txt"
+./testCase6.o "test.txt"
+./testCase7.o "test.txt"
+./testCase8.o "test.txt"
+./testCase9.o "test.txt"
 # ./testCase10.o
 # ./testCase11.o
